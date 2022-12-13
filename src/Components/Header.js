bbd197945dc6544aee2740a5c818bdaa71e9d975
Nícolas <nicolasbrga01@gmail.com>
2022-12-13 18:01:51 -0300

    requisito 3

---
+++ src/Components/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Loading from '../Pages/Loading';
+import { getUser } from '../services/userAPI';
+
+class Header extends Component {
+  state = {
+    loadingPage: false,
+    inputName: '',
+  };
+
+  componentDidMount() { this.getAPI(); }
+
+  async getAPI() {
+    this.setState({ loadingPage: true });
+    const nameLogin = await getUser();
+    this.setState({
+      loadingPage: false,
+      inputName: nameLogin.name });
+  }
+
+  render() {
+    const { loadingPage, inputName } = this.state;
+    return (
+      <header data-testid="header-component">
+        <p data-testid="header-user-name">
+          {loadingPage ? <Loading /> : inputName}
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Header;
